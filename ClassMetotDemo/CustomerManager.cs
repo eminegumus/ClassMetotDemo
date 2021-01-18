839c7d0d930aded044f8330c24fa31dc649097f0
İsmail Gümüşmadenoğlu <igmo@outlook.com.tr>
2021-01-18 18:04:49 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer is Added!");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer is Deleted!");
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Customer is Updated!");
+        }
+
+        public void ListAll(Customer[]customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name+" "+customer.SurName);
+                
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                IsDeleted = false,
+                City = "Ankara",
+                Name = "Emine",
+                SurName = "Gümüş"
+            };
+
+            Customer[] customers=new Customer[]{customer};
+
+            var customerManager=new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Delete(customer);
+            customerManager.Update(customer);
+            customerManager.ListAll(customers);
+            
+        }
+    }
+}
